742de81aa4cb86c534cab11bd1dc112afa55e887
tres <59184373+pybash@users.noreply.github.com>
2026-01-17 17:27:51 -0500

    Remove old server files

--- server/src/routes/timeSign.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-export function GET(req: Request, res: Response) {
-    const currentTime = new Date().toISOString();
-    res.json({ time: currentTime });
-}
