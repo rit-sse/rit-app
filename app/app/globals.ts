1a201bc58c8e0da0772e8188cc7de495dc3cb641
tres <59184373+pybash@users.noreply.github.com>
2026-01-25 10:43:38 -0500

    Navigation Bar Hiding Anim (#15)

--- app/app/globals.ts
+++ app/app/globals.ts
@@ -1,3 +1,13 @@
- export default {
-    navbar: null
-}
+import { Component } from "react";
+
+type Globals = {
+    navbar: { setState: (state: { navBarVisibility: boolean; }) => void; } | null;
+    showNavbar: ((setState: boolean) => void) | null;
+};
+
+const globals: Globals = {
+    navbar: null,
+    showNavbar: null,
+};
+
+export default globals;
