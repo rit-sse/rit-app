78c80f04e7942acb39615d99b93496ca44310c96
Nogardagem <understateddragon@gmail.com>
2026-02-01 11:51:58 -0500

    working map display

--- app/app/map.tsx
+++ app/app/map.tsx
@@ -1,15 +1,12 @@
 import { View, Text } from "react-native";
-import Map from './map/Map';
-
+import Map from "./map/Map";
 
 export default function map() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Map Screen</Text>
 
-      <section className='map-container'>
-         <Map/>
-      </section>
+      <Map/>
     </View>
   );
 }
--- app/app/map/Map.tsx
+++ app/app/map/Map.tsx
@@ -1,13 +1,53 @@
-import { TileLayer , MapContainer  } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Alert } from 'react-native';
+import { Asset } from "expo-asset";
+import { File, Directory, Paths } from 'expo-file-system';
+import { LatLng, LeafletView } from 'react-native-leaflet-view';
 
-export default function Map() {
-  return (
-    <MapContainer center={[0,0]} zoom={ 2 } scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-  </MapContainer>
-  )
+const DEFAULT_LOCATION = {
+  latitude: 43.083,
+  longitude: -77.676
 }
+const Map: React.FC = () => {
+  const [webViewContent, setWebViewContent] = useState<string | null>(null);
+  useEffect(() => {
+    let isMounted = true;
+
+    const loadHtml = async () => {
+      try {
+        const path = require("../../assets/leaflet.html");
+        const asset = Asset.fromModule(path);
+        await asset.downloadAsync();
+        const htmlContent = await (new File(asset.localUri!)).text();//await File.readAsStringAsync(asset.localUri!);
+
+        if (isMounted) {
+          setWebViewContent(htmlContent);
+        }
+      } catch (error) {
+        Alert.alert('Error loading HTML', JSON.stringify(error));
+        console.error('Error loading HTML:', error);
+      }
+    };
+
+    loadHtml();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  if (!webViewContent) {
+    return <ActivityIndicator size="large" />
+  }
+  return (
+    <LeafletView
+      source={{ html: webViewContent }}
+      mapCenterPosition={{
+        lat: DEFAULT_LOCATION.latitude,
+        lng: DEFAULT_LOCATION.longitude,
+      }}
+    />
+  );
+}
+
+export default Map;
