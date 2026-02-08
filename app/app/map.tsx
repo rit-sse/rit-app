53cb24f3e82bee86e2d565bf3e57efdead62560f
Nogardagem <understateddragon@gmail.com>
2026-02-08 12:34:19 -0500

    scroll view for half screen

--- app/app/map.tsx
+++ app/app/map.tsx
@@ -1,11 +1,10 @@
 import React, {useState} from 'react';
-import { View, Text, StyleProp, ViewStyle, Dimensions} from "react-native";
+import { View, Text, StyleProp, ViewStyle, Dimensions, ScrollView} from "react-native";
 import { WebviewLeafletMessage } from 'react-native-leaflet-view';
 import Map from "./map/Map";
 import DragUp from "./DragUp";
 
 import { StyleSheet } from 'react-native';
-import Animated from 'react-native-reanimated';
 import Constants from 'expo-constants';
 
 import GearIcon from "../components/svgs/map/GearIcon";
@@ -38,33 +37,36 @@ export default function map() {
 
       <View style={{position: "absolute", bottom: 35+80+buttonSpacing, right: "5%",
       width: buttonWidth, height: 3.0*buttonWidth+2.0*buttonSpacing}}>
-          <View style={[{bottom: 2.0*(buttonWidth+buttonSpacing)}, allButtonStyling]}>
-            <GearIcon onPress={() => {}} style={iconStyle} fill={"#000"}/>
-          </View>
-
-          <View style={[{bottom: buttonWidth+buttonSpacing}, allButtonStyling]}>
-            <BusIcon onPress={() => {}} style={iconStyle} fill={"#000"}/>
-          </View>
-
-          <View style={[{bottom: 0}, allButtonStyling]}>
-            <BuildingIcon onPress={() => {setModalVisible(true)}} style={iconStyle} fill={"#000"}/>
-          </View>
-          
+        <View style={[{bottom: 2.0*(buttonWidth+buttonSpacing)}, allButtonStyling]}>
+          <GearIcon onPress={() => {}} style={iconStyle} fill={"#000"}/>
         </View>
 
-        <Animated.ScrollView pagingEnabled style={{width:"100%",flex:1}}>
+        <View style={[{bottom: buttonWidth+buttonSpacing}, allButtonStyling]}>
+          <BusIcon onPress={() => {}} style={iconStyle} fill={"#000"}/>
+        </View>
+
+        <View style={[{bottom: 0}, allButtonStyling]}>
+          <BuildingIcon onPress={() => {showBuildingMenu(setModalVisible)}} style={iconStyle} fill={"#000"}/>
+        </View>
+        
+      </View>
+
+      <View style={{width:"100%",height:Dimensions.get('screen').height / 2}}>
+        <ScrollView pagingEnabled scrollEnabled={modalVisible} showsVerticalScrollIndicator={false} style={{width:"100%",height:Dimensions.get('screen').height / 2,flex:1}}>
     {/* so here are your screens, for example first camera second images */}
           {/* <Page title={'PAGE 1 '} index={0} />
           <Page title={'PAGE 2'} index={1} /> */}
-          <View style={{width:"100%",height:Dimensions.get('screen').height - Constants.statusBarHeight,backgroundColor:"#ff000000"}}>
-            <Text style={{}}>hi</Text>
+          <View style={{width:"100%",height:Dimensions.get('screen').height / 2,backgroundColor:"#ff000000"}}>
+            <Text style={{}}>{modalVisible}</Text>
           </View>
-          <View style={{width:"100%",height:Dimensions.get('screen').height - Constants.statusBarHeight,backgroundColor:"#00ff00"}}>
+          <View style={{width:"100%",height:Dimensions.get('screen').height / 2,backgroundColor:"#00ff00"}}>
             <Text style={{}}>hi 2</Text>
           </View>
-        </Animated.ScrollView>
+        </ScrollView>
+      </View>
+        
 
-        <DragUp getVisible={()=>{return modalVisible}} setVisible={setModalVisible}>
+        {/* <DragUp getVisible={()=>{return modalVisible}} setVisible={setModalVisible}>
           <View style={{width:"100%", height:"100%"}}>
             <View style={{height: 6, width: 50, backgroundColor: "#bababa", borderRadius: 5, bottom: 10, left:0, alignSelf:'center'}}>
             </View>
@@ -74,8 +76,12 @@ export default function map() {
             </Text>
           </View>
           
-        </DragUp>
+        </DragUp> */}
 
     </View>
   );
+}
+
+function showBuildingMenu(setModalVisible:Function) {
+  setModalVisible(true);
 }
