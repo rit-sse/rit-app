2e7beeb4fa7e918ac07e3c456f79ffe58c796ccc
tres <59184373+pybash@users.noreply.github.com>
2026-04-14 10:18:29 -0400

    refactor openLink function to comment out AsyncStorage usage; update EventContainer to use TouchableOpacity for navigation

--- app/components/Events/EventContainer.tsx
+++ app/components/Events/EventContainer.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
-import { View, Image, ScrollView, Dimensions } from "react-native";
+import { View, Image, ScrollView, Dimensions, TouchableOpacity } from "react-native";
 import { Text } from "@/components/ui/text";
+import { openLink } from "@/lib/utils";
+import { useRouter } from "expo-router";
 
 export default function EventContainer({ event }: { event: any }) {
-
-  return <View className="flex flex-row items-center w-full my-[10px] ">
+  const route = useRouter()
+  return <TouchableOpacity className="flex flex-row items-center w-full my-[10px] " onPress={() => openLink("https://campusgroups.rit.edu" + event.eventUrlButton, route)}>
     <Image source={{ uri: "https://campusgroups.rit.edu" + event.eventPicture }} className="w-[90px] h-[90px] rounded-lg" />
     <View className="ml-[10px] flex-col justify-between h-[90px]">
       <Text className="font-bold line-clamp-1">{event.eventName}</Text>
       <Text className="text-gray-500">{event.clubName}</Text>
       <Text className="line-clamp-1">{event.eventLocation == "Private Location (sign in to display)" ? "Private Location. Login on site" : event.eventLocation}</Text>
     </View>
-  </View>;
+  </TouchableOpacity>;
 }
