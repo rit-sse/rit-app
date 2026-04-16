708b9aeed0e8ade304b532e9948605210f6a32f9
Cayden Tisdale <caydentisdale43@gmail.com>
2026-04-16 17:54:04 -0400

    fixes

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -3,7 +3,9 @@ import { useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
 import EventsContainer from "@/components/Home/EventsContainer";
 import * as GLOBAL from "./globals";
-import AsyncStorage, {useAsyncStorage} from "@react-native-async-storage/async-storage"
+import AsyncStorage, {
+  useAsyncStorage,
+} from "@react-native-async-storage/async-storage";
 
 import PagerView from "react-native-pager-view";
 import { ButtonCustomWrap } from "@/components/Home/ButtonCustomWrap";
@@ -11,8 +13,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import NewsContainer from "@/components/Home/NewsContainer";
 import RecentlyViewedButton from "@/components/Home/RecentlyViewedButton";
 import { buildApiUrl } from "@/lib/api";
-import { clearRecentlyView, getRecentlyView, gridBox, storeRecentlyView } from "@/lib/utils";
-import { buildApiUrl } from "@/lib/api";
+import {
+  clearRecentlyView,
+  getRecentlyView,
+  gridBox,
+  storeRecentlyView,
+} from "@/lib/utils";
 
 interface NewsArticle {
   uri: string;
@@ -31,9 +37,8 @@ export default function Index() {
   const [recentlyViewed, setRecentlyViewed] = useState<gridBox[]>();
 
   useEffect(() => {
-    getRecentlyView().then((data) => setRecentlyViewed(data.reverse()))
+    getRecentlyView().then((data) => setRecentlyViewed(data.reverse()));
   }, []);
-
 
   const draggableExample = [
     <EventsContainer
@@ -62,17 +67,15 @@ export default function Index() {
 
   useEffect(() => {
     fetch(buildApiUrl("/news"))
-      .then(response => response.json())
-      .then(data => setNews(data["data"]))
-      .catch(error => console.error('Error fetching news:', error));
+      .then((response) => response.json())
+      .then((data) => setNews(data["data"]))
+      .catch((error) => console.error("Error fetching news:", error));
   }, []);
-
 
   return (
     <SafeAreaView style={{ backgroundColor: "#fff" }}>
       <ScrollView
         contentContainerStyle={{
-
           alignItems: "center",
           paddingBottom: 100,
         }}
@@ -114,22 +117,22 @@ export default function Index() {
                 width:
                   scrollOffset - 0.95 < index && index < scrollOffset + 0.95
                     ? Math.max(
-                      30 +
-                      20 *
-                      Math.sin(
-                        Math.PI * (scrollOffset - index) + Math.PI / 2,
-                      ),
-                      10,
-                    )
+                        30 +
+                          20 *
+                            Math.sin(
+                              Math.PI * (scrollOffset - index) + Math.PI / 2,
+                            ),
+                        10,
+                      )
                     : 10,
                 borderRadius: 5,
                 backgroundColor:
                   scrollOffset - 1 < index && index < scrollOffset + 1
                     ? "rgba(247, 105, 2, " +
-                    (3 / 5 +
-                      (2 / 5) *
-                      Math.cos(scrollOffset * Math.PI + Math.PI * index)) +
-                    ")"
+                      (3 / 5 +
+                        (2 / 5) *
+                          Math.cos(scrollOffset * Math.PI + Math.PI * index)) +
+                      ")"
                     : "rgba(247, 105, 2, 0.2)",
                 marginHorizontal: 3,
               }}
@@ -138,27 +141,32 @@ export default function Index() {
         </View>
 
         <View style={{ marginTop: 15, width: "85%" }}>
-
-          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>Recently Viewed</Text>
-          <ScrollView horizontal={true} style={{ width: "100%" }} showsHorizontalScrollIndicator={false}>
+          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>
+            Recently Viewed
+          </Text>
+          <ScrollView
+            horizontal={true}
+            style={{ width: "100%" }}
+            showsHorizontalScrollIndicator={false}
+          >
             {/* {
               Array.from({ length: 10}).map((_, index) => <RecentlyViewedButton key={index} />)
             } */}
-            {
-              recentlyViewed ? recentlyViewed.map((item: gridBox, index: number) => <RecentlyViewedButton key={index} item={item} />) : null
-            }
+            {recentlyViewed
+              ? recentlyViewed.map((item: gridBox, index: number) => (
+                  <RecentlyViewedButton key={index} item={item} />
+                ))
+              : null}
           </ScrollView>
-
         </View>
-            {/* <Button title="Clear recently viewed data" onPress={() => { clearRecentlyView(); setRecentlyViewed(null); }} /> */}
+        {/* <Button title="Clear recently viewed data" onPress={() => { clearRecentlyView(); setRecentlyViewed(null); }} /> */}
         <View style={{ marginTop: 15, width: "85%" }}>
-
-          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>News</Text>
-          {
-            news.map((article, index) => (
-              <NewsContainer article={article} key={index} index={index} />
-            ))
-          }
+          <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>
+            News
+          </Text>
+          {news.map((article, index) => (
+            <NewsContainer article={article} key={index} index={index} />
+          ))}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -166,7 +174,9 @@ export default function Index() {
 }
 
 // Old Debug Functionality
-{/* <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>debug</Text>
+{
+  /* <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 10 }}>debug</Text>
   <Button title={ "Hide Nav Bar"} onPress={() => {GLOBAL.default.navbar?.setState({ navBarVisibility: false })}} />
   <Button title={ "Nav Bar Hiding Animation"} onPress={() => {GLOBAL.default.showNavbar?.(!hiding); setHiding(!hiding);}} />
-  <Button title={ "Goto Dining Search"} onPress={() => {routeNavigator.push("/dining/search")}} /> */}
+  <Button title={ "Goto Dining Search"} onPress={() => {routeNavigator.push("/dining/search")}} /> */
+}
