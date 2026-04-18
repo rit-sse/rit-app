0c4f6c68a6f974dc37d3731e1292d4037760ce92
tres <59184373+pybash@users.noreply.github.com>
2026-04-18 17:10:36 -0400

    Parsing HTML tags

--- webserver/src/routes/events/route.ts
+++ webserver/src/routes/events/route.ts
@@ -1,12 +1,14 @@
 import { ScrapeCache } from "../../db/cache";
 import { Request, Response } from "express";
 
+import * as cheerio from "cheerio";
+
 export const GET = async (req: Request, res: Response) => {
 
     const scrapeCache = new ScrapeCache();
-    if(await scrapeCache.getCache("rit_events") && !(await scrapeCache.isExpired("rit_events"))) {
-        return res.header("Content-Type", "application/json").send(await scrapeCache.getCache("rit_events"));
-    }
+    //if(await scrapeCache.getCache("rit_events") && !(await scrapeCache.isExpired("rit_events"))) {
+    //    return res.header("Content-Type", "application/json").send(await scrapeCache.getCache("rit_events"));
+    //}
 
     const events = await (await fetch("https://campusgroups.rit.edu/mobile_ws/v17/mobile_events_list?range=0&limit=40")).json();
 
@@ -19,9 +21,19 @@ export const GET = async (req: Request, res: Response) => {
             if(fields[x].trim() == '') continue;
             eventData[fields[x]] = event[`p${x}`];
         }
+        if (eventData["eventName"]) {
+            eventData["eventDates"] = cheerio.load(eventData["eventDates"]).text().trim();
+        }
+        if (eventData["eventTags"]) {
+            let allTags =[];
+            for(const tag of cheerio.load(eventData["eventTags"])("a")) {
+                allTags.push(cheerio.load(tag).text().trim());
+            }
+            eventData["eventTags"] = allTags;
+        }
         eventParsed.push(eventData);
     }
-    console.log(eventParsed)
+    console.log(JSON.stringify(eventParsed, null, 2));
     await scrapeCache.setCache("rit_events", eventParsed);
     res.header("Content-Type", "application/json").send(await scrapeCache.getCache("rit_events"));
 }
