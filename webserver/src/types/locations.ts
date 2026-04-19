a1fd7c86ac4c609f5dc525cd4841f62569ff7bb0
Cayden Tisdale <caydentisdale43@gmail.com>
2026-04-19 17:52:33 -0400

    update location types

--- webserver/src/types/locations.ts
+++ webserver/src/types/locations.ts
@@ -1,3 +1,22 @@
+export interface RawLocation {
+  id: number;
+  mdo_id: number;
+  name: string;
+  descShort: string | null;
+  descLong: string | null;
+  phone: string | null;
+  webLink: string | null;
+  abbreviation: string | null;
+  buildingNumber: string | null;
+  thumbnail: string | null;
+  floorLevel: string | null;
+  roomNumber: string | null;
+  hours: string | null;
+  address: string | null;
+  isSearchable: boolean;
+  geometry_id: number | null;
+}
+
 export interface Location {
   id: number;
   mdoId: number;
@@ -11,7 +30,18 @@ export interface Location {
   geometryId: number | null;
 }
 
-export type Position = [number, number]; // [latitude, longitude]
+export interface LocationSearchRecord {
+  mdoId: number;
+  name: string;
+  abbreviation: string | null;
+  buildingNumber: string | null;
+  roomNumber: string | null;
+  primaryLabel: string;
+  secondaryLabel: string | null;
+  searchTokens: string[];
+}
+
+export type Position = [number, number]; // [long, lat]
 
 export interface PointGeometry {
   type: "Point";
@@ -33,6 +63,37 @@ export type LocationGeometry =
   | PolygonGeometry
   | MultiPolygonGeometry;
 
+export interface RawLocationImage {
+  alt: string;
+  url: string;
+  mdo_id: number;
+  location_id: number;
+}
+
+export interface RawLocationFeatureProperties {
+  id: number;
+  mdo_id: number;
+  name: string;
+  descShort?: string | null;
+  descLong?: string | null;
+  phone?: string | null;
+  webLink?: string | null;
+  abbreviation?: string | null;
+  buildingNumber?: string | null;
+  roomNumber?: string | null;
+  hours?: string | null;
+  isSearchable: boolean;
+  geometry_id: number | null;
+  images?: RawLocationImage[];
+  menus?: string[];
+}
+
+export interface RawLocationFeature {
+  type: "Feature";
+  geometry: LocationGeometry;
+  properties: RawLocationFeatureProperties;
+}
+
 export interface LocationFeatureProperties {
   id: number;
   mdoId: number;
@@ -49,4 +110,11 @@ export interface LocationFeature {
   type: "Feature";
   geometry: LocationGeometry;
   properties: LocationFeatureProperties;
+}
+
+export interface EnrichedLocation {
+  location: Location;
+  search: LocationSearchRecord;
+  features: LocationFeature[];
+  labelPoint: Position | null;
 }
