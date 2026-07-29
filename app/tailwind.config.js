ee854d8d8cde0b145071895b6eed2a4b5b92aaf6
tres <59184373+pybash@users.noreply.github.com>
2026-07-29 11:22:06 -0400

    Package and Config changes

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,7 @@ const { hairlineWidth } = require('nativewind/theme');
  
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
